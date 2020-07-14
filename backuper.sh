e620a9143bc1b6f233b47313545740fab5bc8c47
GrygaDenys <49619156+GrygaDenys@users.noreply.github.com>
2020-07-15 01:56:26 +0300

    Create backuper.sh

---
+++ backuper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+conf=$(ls /home/gryga/ -a | grep ".*\.conf$")
+config=$(ls /home/gryga/ -a | grep ".*\.config$")
+
+for var in $conf
+do
+cp /home/gryga/$var /home/gryga/backup/
+done
+
+for var in $config
+do
+cp -R /home/gryga/$var /home/gryga/backup/
+done
+
+echo $1
+lines="* * * * * /home/gryga/Desktop/$1"
+echo "$lines" | crontab -u root -
